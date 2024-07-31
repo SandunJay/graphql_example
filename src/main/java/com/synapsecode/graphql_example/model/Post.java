84b4d16db1e021d41780eb914b63f4c3380d2b77
Sandun Jayawardhana <99672222+SandunJay@users.noreply.github.com>
2024-07-31 06:22:02 +0530

    Update Post.java

--- src/main/java/com/synapsecode/graphql_example/model/Post.java
+++ src/main/java/com/synapsecode/graphql_example/model/Post.java
@@ -1,20 +1,23 @@
 package com.synapsecode.graphql_example.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
-public class Post {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String content;
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User author;
+    private String name;
+    private String description;
+    private double price;
 }
